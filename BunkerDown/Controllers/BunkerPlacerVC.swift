58139f887db38e0fe6f4a864a7cea9f90126d3a6
Andre <aburgoyne@css.edu>
2018-09-17 15:09:44 -0600

    initial commit

--- BunkerDown/Controllers/BunkerPlacerVC.swift
+++ BunkerDown/Controllers/BunkerPlacerVC.swift
@@ -10,7 +10,7 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class BunkerPlacerVC: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
@@ -24,7 +24,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene(named: "art.scnassets/PSP.dae")!
         
         // Set the scene to the view
         sceneView.scene = scene
